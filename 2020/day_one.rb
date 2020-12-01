39bb5da3df945974d49f28fca30e3f7f9d2cb0da
Teapane <tyler.stephen.long@gmail.com>
2020-12-01 07:21:02 -0700

    refactor

--- 2020/day_one.rb
+++ 2020/day_one.rb
@@ -1,24 +1,15 @@
 # Day One:
 # Part One
-def two_sum(file, target)
+def expense_report(file, permutation, target)
   opened_file = open_file(file)
-  opened_file.permutation(2).find do |collection|
+  opened_file.permutation(permutation).find do |collection|
     collection.sum == target
   end.inject(:*)
 end
-
-# Part two
-def three_sum(file, target)
-  opened_file = open_file(file)
-  opened_file.permutation(3).find do |collection|
-    collection.sum == target
-  end.inject(:*)
-end
-
 
 def open_file(file)
   File.open(file, 'r').map(&:to_i)
 end
 
-puts two_sum('2020/day_one_input.txt', 2020) # => 964875
-puts three_sum('2020/day_one_input.txt', 2020) # => 158661360
+puts expense_report('2020/day_one_input.txt', 2, 2020) # => 964875
+puts expense_report('2020/day_one_input.txt', 3, 2020) # => 158661360
