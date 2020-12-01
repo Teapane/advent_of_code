d7260ad59e67a297bead0975fc417d3959f28d4e
Teapane <tyler.stephen.long@gmail.com>
2020-12-01 06:42:47 -0700

    day one 2020

--- 2020/day_one.rb
+++ 2020/day_one.rb
@@ -1,1 +1,24 @@
 # Day One:
+# Part One
+def two_sum(file, target)
+  opened_file = open_file(file)
+  opened_file.permutation(2).find do |collection|
+    collection.sum == target
+  end.inject(:*)
+end
+
+# Part two
+def three_sum(file, target)
+  opened_file = open_file(file)
+  opened_file.permutation(3).find do |collection|
+    collection.sum == target
+  end.inject(:*)
+end
+
+
+def open_file(file)
+  File.open(file, 'r').map(&:to_i)
+end
+
+puts two_sum('2020/day_one_input.txt', 2020)
+puts three_sum('2020/day_one_input.txt', 2020)
